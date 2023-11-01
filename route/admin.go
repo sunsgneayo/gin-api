1951f0489b96f945e97db01f84b3cbc7a8c8b381
sunagne <583985775@qq.com>
2023-11-01 14:18:21 +0800

    upload

--- route/admin.go
+++ route/admin.go
@@ -7,10 +7,16 @@ import (
 
 func CommonRouter(r *gin.Engine) *gin.Engine {
 
+	// 注册
 	r.POST("register", controller.Register)
+
+	// 登录
 	r.POST("login", controller.Login)
+
+	// 获取验证码
 	r.GET("captcha", controller.Captcha)
 
+	// 文件上传
 	r.POST("upload", controller.UploadFile)
 
 	return r
